ca37d59620ae37687b468b0114d3d4cb133e8be4
annallanza <annallanza@hotmail.com>
2021-03-08 12:44:05 +0100

    CRUD requests usuari

--- BarGoBack/src/main/java/BarGo/Back/Rest/UsuariRest.java
+++ BarGoBack/src/main/java/BarGo/Back/Rest/UsuariRest.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController //Indiquem que aquesta classe sera un SERVICE REST
 @RequestMapping("usuaris") //Definim la URL del SERVICE (arrel)
@@ -28,6 +26,19 @@ public class UsuariRest {
     @RequestMapping(method = RequestMethod.GET)
     private ResponseEntity<List<Usuari>> getAllUsuaris(){
         return ResponseEntity.ok(usuariService.findAll());
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET) //Exemple url request: http://localhost:8080/usuaris/3
+    private ResponseEntity<Usuari> getUsuariById(@PathVariable("id") Long id){
+        Optional<Usuari> optionalusuari = usuariService.findById(id);
+
+        if (optionalusuari.isPresent()) {
+            Usuari usuari = optionalusuari.get();
+            return ResponseEntity.ok(usuari);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @RequestMapping(method = RequestMethod.POST) //Exemple url request: http://localhost:8080/usuaris
@@ -45,6 +56,40 @@ public class UsuariRest {
         } catch (URISyntaxException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
+    }
+
+    @RequestMapping(method = RequestMethod.PUT) //Exemple url request: http://localhost:8080/products
+    //+ afegir body raw amb format JSON:
+    //{
+    //    "id": 6,
+    //    "nom": "username",
+    //    "contrasenya": "123"
+    //}
+    private ResponseEntity<Usuari> updateUsuari(@RequestBody Usuari usuari) {
+        Optional<Usuari> optionalUsuari = usuariService.findById(usuari.getId());
+        if (optionalUsuari.isPresent()) {
+            Usuari usuariexists = optionalUsuari.get();
+            usuariexists.setNom(usuari.getNom());
+            usuariexists.setContrasenya(encoder.encode(usuari.getContrasenya()));
+            Usuari usuariupdated = usuariService.save(usuariexists);
+            return ResponseEntity.ok(usuariupdated);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE) //Exemple url request: http://localhost:8080/usuaris/3
+    private ResponseEntity<Void> deleteUsuariById(@PathVariable("id") Long id) {
+        Optional<Usuari> optionalUsuari = usuariService.findById(id);
+
+        if(optionalUsuari.isPresent()) {
+            usuariService.deleteById(id);
+
+            Optional<Usuari> optionalUsuariDeleted = usuariService.findById(id);
+            if(!optionalUsuariDeleted.isPresent()) return ResponseEntity.ok(null);
+            else return ResponseEntity.notFound().build();
+        }
+        else return ResponseEntity.badRequest().build();
     }
 
 }
--- BarGoBack/src/main/java/BarGo/Back/SecurityConfig.java
+++ BarGoBack/src/main/java/BarGo/Back/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@Configuration
+@Configuration //TODO: NO CAL?
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
