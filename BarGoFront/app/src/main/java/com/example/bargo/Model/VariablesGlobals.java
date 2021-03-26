64f11d197229b00cc544e10004e7c3272533f4ed
annallanza <annallanza@hotmail.com>
2021-03-26 18:15:55 +0100

    Organitzar i login

---
+++ BarGoFront/app/src/main/java/com/example/bargo/Model/VariablesGlobals.java
@@ -0,0 +1,17 @@
+package com.example.bargo.Model;
+
+public class VariablesGlobals {
+
+    public static String urlAPI = "http://192.168.1.157:8080/";
+
+    public VariablesGlobals() {
+    }
+
+    public static String getUrlAPI() {
+        return urlAPI;
+    }
+
+    public static void setUrlAPI(String urlAPI) {
+        VariablesGlobals.urlAPI = urlAPI;
+    }
+}
